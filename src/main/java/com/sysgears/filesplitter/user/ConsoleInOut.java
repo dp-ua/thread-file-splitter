6bb845da6f4ed517ab96c3e67777e8780c7b6d22
Pavel Reshetilo <pavel.reshetilo@sysgears.com>
2018-08-10 13:03:32 +0300

    console realizations of userInOut interface

---
+++ src/main/java/com/sysgears/filesplitter/user/ConsoleInOut.java
@@ -0,0 +1,33 @@
+package com.sysgears.filesplitter.user;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Communicating with the user via the console
+ */
+public class ConsoleInOut implements UserInOut {
+
+    /**
+     * Write message to console
+     *
+     * @param message sends to console
+     */
+    public void write(String message) {
+        System.out.println(message);
+    }
+
+    /**
+     * Read message from console
+     *
+     * @return string message from user
+     * @throws IOException if there were problems entering
+     */
+    public String read() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
+    }
+
+
+}
