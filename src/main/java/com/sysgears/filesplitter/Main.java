891ec1a1840954923a349ed853344051794cbf2a
Pavel Reshetilo <pavel.reshetilo@sysgears.com>
2018-08-14 12:16:27 +0300

    commit work main class

--- src/main/java/com/sysgears/filesplitter/Main.java
+++ src/main/java/com/sysgears/filesplitter/Main.java
@@ -1,8 +1,10 @@
 package com.sysgears.filesplitter;
 
+import com.sysgears.filesplitter.command.BlockCount;
 import com.sysgears.filesplitter.command.CommandParser;
 import com.sysgears.filesplitter.command.CommandType;
 import com.sysgears.filesplitter.command.BlockSize;
+import com.sysgears.filesplitter.file.FileWriter;
 import com.sysgears.filesplitter.statistic.ThreadInformation;
 import com.sysgears.filesplitter.statistic.TimeController;
 import com.sysgears.filesplitter.user.ConsoleInOut;
@@ -10,101 +12,103 @@ import com.sysgears.filesplitter.user.UserView;
 
 import java.io.*;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        /*try {
-
-
-            FileInputStream inputStream = new FileInputStream("src/main/resources/test.txt");
-            FileOutputStream outputStream = new FileOutputStream("src/main/resources/out.txt",true);
-
-            FileChannel chInput = inputStream.getChannel();
-            System.out.println(chInput.size());
-
-            FileChannel chOutput = outputStream.getChannel();
-            chOutput.transferFrom(chInput,2,1);
-            chOutput.transferFrom(chInput,1,1);
-            chOutput.transferFrom(chInput,0,1);
-
-
-            chOutput.close();
-            chInput.close();
-            outputStream.close();
-            inputStream.close();
-
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (true) return;
-*/
-
         try {
-            UserView userView = new UserView(new ConsoleInOut());
+            ConsoleInOut consoleInOut = new ConsoleInOut();
+            UserView userView = new UserView(consoleInOut);
 
-            while (true) {
-                try {
-                    String input = userView.get();
-                    Thread thread = new Thread(new ThreadGroup("bla"), new Runnable() {
-                        public void run() {
-                            System.out.println("Ваще другой поток " + Thread.currentThread().getName());
-                        }
-                    }, input);
-                    thread.start();
-
-                    Map<Thread, StackTraceElement[]> threads = Thread.getAllStackTraces();
-                    System.out.println("Number of currently running threads: " + threads.size());
-                    for (Thread t : threads.keySet()) {
-                        System.out.println(t);
-                        
-                    }
-
-                    CommandParser commandParser = new CommandParser(input);
-                    if (commandParser.getCommand() == CommandType.EXIT) break;
-                    if (commandParser.getCommand() == CommandType.SPLIT) {
-                        Map<String, String> arguments = commandParser.getArguments();
-                        if (!arguments.containsKey("-p") | !arguments.containsKey("-s"))
-                            throw new IllegalArgumentException("Неправильно указаны аргументы");
-
-                        BlockSize blockSize = new BlockSize(arguments.get("-s"));
-                        String sourceFileName = arguments.get("-p");
-                        FileChannel inChannel;
-
-                        try {
-                            FileInputStream inputStream = new FileInputStream(sourceFileName);
-                            inChannel = inputStream.getChannel();
-                        } catch (FileNotFoundException e) {
-                            throw new FileNotFoundException("Исходный файл не существует");
-                        }
-
-                        if (inChannel.size() < blockSize.getSize()) {
-                            userView.send("Разбивка невозможна. Размер блока больше размера файла. ");
-                            continue;
-                        }
-
-
-                        ThreadInformation threadInformation = new ThreadInformation();
-                        TimeController timeController = new TimeController();
-
-
-                    }
-
-                } catch (FileNotFoundException e) {
-                    userView.send("Ошибка: " + e.getMessage());
-                } catch (NoSuchFieldException e) {
-                    userView.send("Ошибка: " + e.getMessage());
-                } catch (IllegalArgumentException e) {
-                    userView.send("Ошибка: " + e.getMessage());
-                } catch (Exception e) {
-                    e.printStackTrace();
+            while (true) try {
+                userView.send("Input command:");
+                String input = userView.get().trim();
+                CommandParser commandParser = new CommandParser(input);
+                if (commandParser.getCommand() == CommandType.EXIT) break;
+                if (commandParser.getCommand() == CommandType.THREADS) {
+                    userView.send("Потоков: " + Thread.getAllStackTraces().size());
+                    for (Thread t : Thread.getAllStackTraces().keySet())
+                        userView.send(t.toString());
                 }
+                if (commandParser.getCommand() == CommandType.SPLIT) {
+                    Map<String, String> arguments = commandParser.getArguments();
+                    if (!arguments.containsKey("-p") | !arguments.containsKey("-s"))
+                        throw new IllegalArgumentException("Wrong arguments");
+
+                    BlockSize blockSize = new BlockSize(arguments.get("-s"));
+                    String sourceFileName = arguments.get("-p");
+                    FileChannel inChannel;
+
+                    try {
+                        FileInputStream inputStream = new FileInputStream(sourceFileName);
+                        inChannel = inputStream.getChannel();
+                    } catch (FileNotFoundException e) {
+                        throw new FileNotFoundException("Source file does not exist");
+                    }
+
+                    if (inChannel.size() < blockSize.getSize()) {
+                        userView.send("The splitting is impossible. The block size is larger than the file size. ");
+                        continue;
+                    }
+                    BlockCount blockCount = new BlockCount(blockSize.getSize(), inChannel.size());
+                    inChannel.close();
+
+                    /* строки для тестирования
+                    split -p /home/pavel/IdeaProjects/test/test.file -s 200M
+                    split -p /home/pavel/IdeaProjects/test/test3.file -s 800M
+                    */
+
+                    TimeController timeController = new TimeController();
+                    ThreadInformation threadInformation = new ThreadInformation(timeController, consoleInOut);
+                    Thread statistic = new Thread(threadInformation);
+                    statistic.setPriority(8);
+                    statistic.setDaemon(true);
+                    statistic.start();
+
+                    ExecutorService executorService = Executors.newFixedThreadPool(4);
+                    ArrayList<Future<String>> result = new ArrayList<Future<String>>();
+
+                    for (int i = 0; i < blockCount.getCount(); i++) {
+                        String partName = String.format("%0" + blockCount.getDimension() + "d", i + 1);
+                        String threadName = "Thread-" + partName;
+
+                        threadInformation.put(threadName, "start");
+                        File inputFile = new File(sourceFileName);
+                        File outputFile = new File(sourceFileName + partName);
+                        result.add(executorService.submit(new FileWriter(inputFile, outputFile, blockSize.getSize() * i, blockSize.getSize(), threadName,false)));
+                    }
+                    for (Future<String> fs : result)
+                        try {
+                            String[] s = fs.get().split(":");
+                            threadInformation.put(s[0], s[1]);
+
+                        } catch (InterruptedException e) {
+                            userView.send(e.getMessage());
+                        } catch (ExecutionException e) {
+                            userView.send(e.getMessage());
+                        } finally {
+                            executorService.shutdown();
+                        }
+                    statistic.interrupt();
+
+                    userView.send("Splitting complete. Time remaining: " + timeController.getRemainingInSec() + "s");
+                    inChannel.close();
+                } else userView.send("Command not recognized");
+
+            } catch (FileNotFoundException e) {
+                userView.send("Error: " + e.getMessage());
+            } catch (NoSuchFieldException e) {
+                userView.send("Error: " + e.getMessage());
+            } catch (IllegalArgumentException e) {
+                userView.send("Error: " + e.getMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
         } catch (Throwable e) {
