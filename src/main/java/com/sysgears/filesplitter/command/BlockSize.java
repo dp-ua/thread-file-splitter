7ae8ca9e91af7ea8af7617bfa60b462144b4e6be
Pavel Reshetilo <pavel.reshetilo@sysgears.com>
2018-08-14 12:00:10 +0300

    write docs

--- src/main/java/com/sysgears/filesplitter/command/BlockSize.java
+++ src/main/java/com/sysgears/filesplitter/command/BlockSize.java
@@ -1,37 +1,57 @@
 package com.sysgears.filesplitter.command;
 
+/**
+ * Translate the entered block size value into bytes.
+ * <p>
+ * Supports such size units as:
+ * G - Gigabyte
+ * M - megabyte
+ * K - Kilobytes.
+ */
 public class BlockSize {
-    String blockSize;
+
+    /**
+     * Input string with information about the size of the block.
+     * <p>
+     * Сan contain only  numbers or numbers with a letter at the end of the line.
+     */
+    private final String blockSize;
 
     public BlockSize(String blockSize) {
         this.blockSize = blockSize;
     }
 
-    public long getSize() throws NumberFormatException{
+    /**
+     * Return size of block in bytes.
+     *
+     * @return long size of bytes
+     * @throws NumberFormatException if cant parse input string
+     */
+    public long getSize() throws NumberFormatException {
         long size;
         try {
             size = Long.parseLong(blockSize);
             return size;
         } catch (NumberFormatException e) {
+            try {
+                size = Long.parseLong(blockSize.substring(0, blockSize.length() - 1));
+            } catch (NumberFormatException ee) {
+                throw new NumberFormatException("Ошибка ввода размера блока");
+            }
         }
-        try {
-            size = Long.parseLong(blockSize.substring(0, blockSize.length() - 1));
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException("Ошибка ввода размера блока");
-        }
-
 
         long mult = 1;
         switch (blockSize.charAt(blockSize.length() - 1)) {
             case 'G':
-                mult *= 1000;
+                mult *= 1024;
             case 'M':
-                mult *= 1000;
+                mult *= 1024;
             case 'K':
-                mult *= 1000;
+                mult *= 1024;
                 break;
-                default:throw new NumberFormatException("Не удалось распознать размерность блока");
+            default:
+                throw new NumberFormatException("Не удалось распознать размерность блока");
         }
-        return size*mult;
+        return size * mult;
     }
 }
