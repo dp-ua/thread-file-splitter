b1a773e0bf304b2320dfee5f6d9046fb4e4705cb
Pavel Reshetilo <pavel.reshetilo@sysgears.com>
2018-08-20 16:18:35 +0300

    add descriptions and write docs

--- src/main/java/com/sysgears/filesplitter/command/CommandType.java
+++ src/main/java/com/sysgears/filesplitter/command/CommandType.java
@@ -4,13 +4,18 @@ package com.sysgears.filesplitter.command;
  * List of available commands
  */
 public enum CommandType {
-    SPLIT,
-    EXIT,
-    HELP,
-    BLANK,
-    ERROR,
-    THREADS,
+    SPLIT("разбивка файла на части"),
+    EXIT("выход"),
+    BLANK("введена пустая строка"),
+    ERROR("команда задана не верно"),
+    THREADS("список потоков"),
     ;
+    private String description;
+
+    CommandType(String description) {
+
+        this.description = description;
+    }
 }
 
 
