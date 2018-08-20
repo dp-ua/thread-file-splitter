47d20496d43f28cd306ae1160e889729bd2b8a6f
Pavel Reshetilo <pavel.reshetilo@sysgears.com>
2018-08-20 16:23:37 +0300

    write docs

--- src/main/java/com/sysgears/filesplitter/command/CommandParser.java
+++ src/main/java/com/sysgears/filesplitter/command/CommandParser.java
@@ -1,35 +1,74 @@
 package com.sysgears.filesplitter.command;
 
+import com.sysgears.filesplitter.Exception.MyException;
+import com.sysgears.filesplitter.Exception.TypeException;
+
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Parser for input String line
+ */
 public class CommandParser {
-    String[] args;
+    /**
+     * Line for parse
+     */
+    private String[] args;
 
+    /**
+     * Set line for parsing
+     *
+     * @param input string for parse
+     */
+    public void setArgs(String input) {
+        if (input == null) input = "";
+        this.args = input.split(" ");
+    }
+
+    /**
+     * Blank constructor
+     */
+    public CommandParser() {
+    }
+
+    /**
+     * Initialize and set string for parsing
+     *
+     * @param args input string for parse
+     */
     public CommandParser(String args) {
         this.args = args.split(" ");
     }
 
-
+    /**
+     * Get type of command
+     *
+     * @return type of command in CommandType. If cant parsing command - return CommandType.ERROR
+     */
     public CommandType getCommand() {
-        if (args.length==0) return CommandType.BLANK;
+        if (args.length == 0) return CommandType.BLANK;
 
         try {
-        CommandType type = CommandType.valueOf(args[0].toUpperCase());
-        return type;
-        }catch (IllegalArgumentException e) {
+            return CommandType.valueOf(args[0].toUpperCase());
+        } catch (IllegalArgumentException e) {
             return CommandType.ERROR;
         }
     }
 
-    public Map<String,String> getArguments() throws NoSuchFieldException,IllegalArgumentException {
-        Map <String, String> map = new HashMap<String, String>();
-        if(args.length<=1) throw new NoSuchFieldException("Отсутствуют аргументы");
-        if ((args.length-1)%2!=0)  throw new IllegalArgumentException("Неверно заданы аргументы");
+    /**
+     * Get arguments from input string
+     *
+     * @return Map of arguments
+     * @throws MyException if cant parse arguments
+     */
+    public Map<String, String> getArguments() throws MyException {
+        Map<String, String> map = new HashMap<>();
+        if (args.length <= 1) throw new MyException(TypeException.WRONGARG);
+        if ((args.length - 1) % 2 != 0) throw new MyException(TypeException.WRONGARG);
 
-        for (int i = 1; i < args.length; i=i+2) {
-            if (args[i].charAt(0)!='-') throw new IllegalArgumentException("Неверно заданы аргументы");
-            map.put(args[i],args[i+1]);
+        for (int i = 1; i < args.length; i = i + 2) {
+            if (args[i].charAt(0) != '-') throw new MyException(TypeException.WRONGARG);
+            map.put(args[i], args[i + 1]);
 
         }
         return map;
