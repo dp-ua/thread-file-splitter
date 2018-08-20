a7657bdb251186671aa9bc0ede50329d0c9725e8
Pavel Reshetilo <pavel.reshetilo@sysgears.com>
2018-08-20 16:15:50 +0300

    write docs

--- src/main/java/com/sysgears/filesplitter/statistic/AbstractStatistic.java
+++ src/main/java/com/sysgears/filesplitter/statistic/AbstractStatistic.java
@@ -4,14 +4,49 @@ import java.util.Map;
 
 /**
  * Asbract statistics.
- *
+ * <p>
  * Information about methods for working with statistics
  */
 public interface AbstractStatistic {
-    Map<String,String> getAll();
+    /**
+     * Get all elements
+     *
+     * @return Map<String   ,   String>
+     * key - name of thread
+     * value - progress
+     */
+    Map<String, String> getAll();
+
+    /**
+     * Put some record to map
+     * @param key name of thread
+     * @param value progress
+     * @return true if operation was successful
+     */
     boolean put(String key, String value);
+
+    /**
+     * Get value by key
+     *
+     * @param key name of thread
+     * @return value progress of thread
+     */
     String get(String key);
+
+    /**
+     * Clear map
+     */
     void clearAll();
+
+    /**
+     * Get trigger of done
+     * @return true if all done
+     */
     boolean isDone();
+
+    /**
+     * Set triiger done
+     * @param done boolean
+     */
     void setDone(boolean done);
 }
