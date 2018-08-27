da48a5f73df885ca863bd445fc79655e13c65338
Pavel Reshetilo <pavel.reshetilo@sysgears.com>
2018-08-27 15:09:03 +0300

    write docs

--- src/main/java/com/sysgears/filesplitter/file/data/copy/StreamDataCopying.java
+++ src/main/java/com/sysgears/filesplitter/file/data/copy/StreamDataCopying.java
@@ -1,11 +1,11 @@
-package com.sysgears.filesplitter.file.block.movers;
+package com.sysgears.filesplitter.file.data.copy;
 
 import com.sysgears.filesplitter.statistic.AbstractStatistic;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.channels.FileChannel;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
 
 /**
  * A streaming method for writing a piece of data from one file to another.
@@ -14,62 +14,28 @@ import java.util.concurrent.*;
  * Displays information about the flow progress in the name of the thread
  * It can be used both for dividing a file into parts and for collecting a file into one whole
  */
-public class BigBlockMover implements Callable<String> {
+public class StreamDataCopying implements Callable<String> {
+    private int blockSize = 1024;
 
-    /**
-     * Statistic holder.
-     */
     private final AbstractStatistic statistic;
-
-
-    /**
-     * Source file, we take the data from it
-     */
     private final File sourceFile;
-
-    /**
-     * The recipient file, we put the data into it
-     */
     private final File outputFile;
-
-    /**
-     * The initial position in the source file from where we start to take data
-     */
     private final long startPosInSource;
-
-    /**
-     * The initial position in the output file from where we start to take data
-     */
     private final long startPosInOutput;
-
-    /**
-     * Size of the data block to copy.
-     * <p>
-     * The block size can be specified more than the size of the available data in the file.
-     */
     private final long size;
-
-    /**
-     * Thread name.
-     */
     private final String threadName;
-
-    /**
-     * If the value is false, the streaming method does not work with the statistics and does not enter data into it
-     */
-    private boolean useStatistic;
 
     /**
      * Set basic params for work with statistic
      *
-     * @param sourceFile       - source file
-     * @param outputFile       - distanation file
+     * @param sourceFile       - source file. From it we take the data
+     * @param outputFile       - output file. In it we put the data
      * @param startPosInSource - start posoition in source file
-     * @param size             - size of block to copy.
+     * @param size             - data size for copying
      * @param threadName       - thread name
      * @param statistic        - statistic holder
      */
-    public BigBlockMover(File sourceFile, File outputFile, long startPosInSource, long startPosInOutput, long size, String threadName,  AbstractStatistic statistic) {
+    public StreamDataCopying(File sourceFile, File outputFile, long startPosInSource, long startPosInOutput, long size, String threadName, AbstractStatistic statistic) {
         this.sourceFile = sourceFile;
         this.startPosInSource = startPosInSource;
         this.startPosInOutput = startPosInOutput;
@@ -77,10 +43,20 @@ public class BigBlockMover implements Callable<String> {
         this.threadName = threadName;
         this.outputFile = outputFile;
         this.statistic = statistic;
-        useStatistic = true;
     }
 
-      public String call() throws Exception {
+    /**
+     * Copies a block of data from source file  to output file.
+     * <p>
+     * For the operation of statistics, the function of copying a large piece of data is divided into small parts.
+     * As a result of the execution of the flow, the result is recorded in the statistics, that 100% of the data is copied.
+     *
+     * @return returns a string of type: "Threadename:done"
+     * @throws Exception all exceptions related to the initial position, size and availability of the file
+     *                   must be provided in the calling function.
+     *                   If something went wrong - an exception will be thrown.
+     */
+    public String call() throws Exception {
         statistic.put(threadName, "start");
 
         FileChannel inputChannel = new FileInputStream(sourceFile).getChannel();
@@ -88,23 +64,21 @@ public class BigBlockMover implements Callable<String> {
             throw new IllegalArgumentException("Start positions is out of file");
         statistic.put(threadName, "0");
 
-        int blockSize = 1024;
         long posInSource = startPosInSource;
         long posInOutput = startPosInOutput;
         long workSize = (startPosInSource + size) > inputChannel.size() ? inputChannel.size() - startPosInSource - 1 : size;
         long done = 0;
 
-        SmallBlockMover smallBlockMover=new SmallBlockMover();
-        int count=0;
+        SmallBlockCopying smallBlockCopying = new SmallBlockCopying();
+
         while (workSize > 0) {
-            count++;
-            blockSize = workSize < blockSize ? (int) workSize : 1024;
-            int written = smallBlockMover.move(inputChannel, posInSource, blockSize, outputFile, posInOutput);
+            int tempSize = workSize < blockSize ? (int) workSize : blockSize;
+            int written = smallBlockCopying.move(inputChannel, posInSource, tempSize, outputFile, posInOutput);
             done += written;
-            statistic.put(threadName, String.valueOf((long)(((double)done/size)*100)));
-            posInSource += blockSize;
-            posInOutput += blockSize;
-            workSize -= blockSize;
+            statistic.put(threadName, String.valueOf((long) (((double) done / size) * 100)));
+            posInSource += tempSize;
+            posInOutput += tempSize;
+            workSize -= tempSize;
             Thread.yield();
         }
         inputChannel.close();
